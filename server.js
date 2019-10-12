efa603089beb9cfa2f7c4ab42bc265a1e03c6b2a
Henrique <henrique.1360@gmail.com>
2019-10-12 15:59:59 -0300

    Syscoin REST application

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+app.get("/provasyscoin", (req, res) => {
+    const nome = 'Henrique Leal Araújo';
+    const user = req.query.user;
+    const password = req.query.password;
+    if(req.query.user === 'syscoin' && req.query.password === 'meEscolhe') {
+
+    res.json({mensagem: `Meu nome completo é ${nome}. Eu quero essa vaga!`});
+    
+  } else {
+    res.json({mensagem: `Credenciais Erradas`});
+  }
+});
+
+app.listen(3000, function() {
+});
